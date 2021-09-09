c1e104e827e678afc39bb0bd758247eadf72042d
JJLindsey <jlindsey010@gmail.com>
2021-09-09 10:37:50 -0700

    user & login mutation functionality

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+import { useQuery, UseMutation } from '@apollo/client'
+import { GET_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { REMOVE_BOOK } from '../utils/mutations'
 
 const SavedBooks = () => {
-    const [userData, setUserData] = useState({});
+    const {loading, data } = useQuery(GET_ME);
+    const [removeBook] = UseMutation(REMOVE_BOOK, )
 
     // use this to determine if `useEffect()` hook needs to run again
     const userDataLength = Object.keys(userData).length;
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from '@apolloclient'
+import {SAVED_BOOKS} from '../utils/mutations'
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/mutations';
+import { searchGoogleBooks } from '../utils/mutations';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
