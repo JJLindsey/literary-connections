03926eb4e508070552c86587d517fc4d11ba326f
JJLindsey <jlindsey010@gmail.com>
2021-09-10 09:36:37 -0700

    update package JSON

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
-
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 //const routes = require('./routes');
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
     typeDefs, resolvers,
-    context: authMiddleware
+    context: authMiddleware,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
 })
 
 //integrate Apollo w/ Express applications as middleware
